2d902595b8fec5fc4c6f1c339c5fd02835ee1068
Coetzee van Staden <coetzee.v@superbalist.com>
2019-04-24 07:35:19 +0200

    added methods to create datasources on load and refresh - to list employee data

--- manager/src/components/EmployeeList.js
+++ manager/src/components/EmployeeList.js
@@ -1,12 +1,26 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Text, View } from 'react-native';
+import { ListView, Text, View } from 'react-native';
 import { employeeFetch } from '../actions';
 
 class EmployeeList extends Component {
   componentWillMount() {
     this.props.employeeFetch();
+    this.createDataSource(this.props);
   }
+
+  componentWillReceiveProps(nextProps) {
+    this.createDataSource(nextProps);
+  }
+
+  createDataSource({ employees }) {
+    const ds = new ListView.dataSource({
+      rowHasChanged: (r1, r2) => r1 !== r2
+    });
+
+    this.dataSource = ds.cloneWithRows(employees);
+  }
+
 
   render() {
     return (
