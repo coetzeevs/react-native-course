00b17571ab009927fcfa54d516532e86ad2534e2
Coetzee van Staden <coetzee.v@superbalist.com>
2019-04-25 09:16:32 +0200

    installed new package and enabled text-from-app

--- manager/src/components/EmployeeEdit.js
+++ manager/src/components/EmployeeEdit.js
@@ -1,6 +1,7 @@
 import _ from 'lodash';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { text } from 'react-native-communications';
 import EmployeeForm from './EmployeeForm';
 import { employeeUpdate, shiftSelectError, employeeSave } from '../actions';
 import { Card, CardSection, Button } from './common';
@@ -23,6 +24,12 @@ class EmployeeEdit extends Component {
     }
   }
 
+  onTextPress() {
+    const { name, phone, shift } = this.props;
+
+    text(phone, `Hey ${name}, \nYour upcoming shift is on ${shift}`);
+  }
+
   render() {
     return (
       <Card>
@@ -30,6 +37,12 @@ class EmployeeEdit extends Component {
         <CardSection>
           <Button onPress={this.onButtonPress.bind(this)}>
             Save
+          </Button>
+        </CardSection>
+
+        <CardSection>
+          <Button onPress={this.onTextPress.bind(this)}>
+            Text Schedule
           </Button>
         </CardSection>
       </Card>
